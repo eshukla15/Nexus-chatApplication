3adc92781bee7127788532f5d3b2d4b6c8b88a4f
eshukla15 <eshukla15@gmail.com>
2025-04-22 22:57:13 +0530

    Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,12 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js"
 import { connectDB } from "./lib/db.js";
+import path from "path";
 
 dotenv.config();                    // lets us load content of dotenv
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // extract the json the data from body of request
 app.use(express.json());
@@ -26,6 +28,15 @@ app.use("/api/auth", authRoutes);
 
 //route for messages
 app.use("/api/messages", messageRoutes);
+
+//through this we're serving frontend and backend to same server
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get(("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+}));
+}
 
 server.listen(PORT, () => {
     console.log("server is running on " +PORT);
