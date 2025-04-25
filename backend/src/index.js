3cefc2ae4b4a9fc448f99e973af21536b01c6045
eshukla15 <eshukla15@gmail.com>
2025-04-25 20:57:30 +0530

    Update profile glitch removed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,14 +14,18 @@ dotenv.config();                    // lets us load content of dotenv
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// extract the json the data from body of request
-app.use(express.json());
+
 app.use(cookieParser());
 
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }));
+
+// extract the json the data from body of request
+// Increase payload limit (to fix 413 error)
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // route for authorization
 app.use("/api/auth", authRoutes);
