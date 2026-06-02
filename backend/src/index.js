f19b08c61577be14b1f0cc0540ace6bebca861fb
Eshan Shukla <eshan.shukla@codestoresolutions.com>
2026-06-02 15:40:33 +0530

    added docker file

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const __dirname = path.resolve();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4000"],
     credentials: true
 }));
 
